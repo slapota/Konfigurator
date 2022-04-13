b9fe54025d58cdfe120698f98979d442ae0b2215
slapota <arnost.spalensky@gmail.com>
2022-04-13 15:43:11 +0200

    nevim

---
+++ Konfigurátor/Assets/Scripts/Menu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DanielLochner.Assets.SimpleScrollSnap
+{
+    public class Menu : MonoBehaviour
+    {
+        public GameObject[] panels, objects;
+        public SimpleScrollSnap ScrollSnap;
+        public Material body;
+        int i = 0;
+
+        void Start()
+        {
+            if(body != null)
+            {
+                ChangeColor();
+            }
+            ScrollSnap = ScrollSnap.GetComponent<SimpleScrollSnap>();
+            foreach (var item in panels)
+            {
+                ScrollSnap.Add(item, ScrollSnap.Content.childCount);
+            }
+        }
+        public void Change()
+        {
+            foreach (var item in objects)
+            {
+                item.SetActive(false);
+            }
+            objects[i].SetActive(true);
+        }
+        public void ChangeColor()
+        {
+            body.color = panels[i].GetComponent<Image>().color;
+        }
+        public void Next()
+        {
+            i++;
+            if (i < 0)
+            {
+                i = objects.Length - 1;
+            }
+            else if (i > objects.Length - 1)
+            {
+                i = 0;
+            }
+        }
+        public void Previous()
+        {
+            i--;
+            if (i < 0)
+            {
+                i = objects.Length - 1;
+            }
+            else if (i > objects.Length - 1)
+            {
+                i = 0;
+            }
+        }
+    }
+}
